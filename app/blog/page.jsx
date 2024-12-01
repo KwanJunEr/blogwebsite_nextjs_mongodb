98437b8fdfe9fe9a27e814f77192f0ec24df790e
KwanJunEr <jonastheitguy@gmail.com>
2024-12-02 06:40:50 +0800

    completed blogpage frontend and setup for singlepastpost with slug

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import styles from "./blogPage.module.css"
+import Menu from '../../components/Menu/Menu'
+import CardList from '../../components/cardlist/CardList'
 
 const BlogPage = () => {
   return (
-    <div>
-      
+    <div className={styles.container}>
+      <h1 className={styles.title}>Style Blog</h1>
+      <div className={styles.content}> 
+        <CardList/>
+        <Menu/>
+      </div>
     </div>
   )
 }
